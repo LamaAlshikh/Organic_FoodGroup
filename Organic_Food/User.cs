6241868f428d5e80468d3864f188863a3167324f
LamaAlshikh <lalshikh@stu.kau.edu.sa>
2024-11-12 00:07:45 +0300

    User.cs

---
+++ Organic_Food/User.cs
@@ -0,0 +1,9 @@
+Create namespace Organic_foods.Models
+{     
+         public class User
+    {
+        public int ID { get; set; }
+        public string username { get; set; }
+        public string password { get; set; }
+    } 
+    }
